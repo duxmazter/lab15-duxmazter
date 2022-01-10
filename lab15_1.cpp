355d51d6b88457be9850395dc18e51a8a5c9dac1
duxmazter <46duxmazter@gmail.com>
2022-01-11 00:11:51 +0700

    fin

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,35 +3,32 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-    int i, temp, j; 
-    for (i = 1; i < N; i++)
+    for (int i = 1; i < N; i++)
     { 
-        temp = d[i]; 
-        j = i - 1; 
+        int temp = d[i]; 
+        int j = i - 1; 
 
 
-        while (j >= 0 && d[j] < temp)
+        while (j >= 0 && temp > d[j])
         { 
             d[j + 1] = d[j]; 
-            j --; 
+            j--; 
         } 
         d[j + 1] = temp; 
-		cout <<"Pass "<<i<<":";
-        for(int i = 0; i < 10; i++) cout << d[i] << " ";
+        cout <<"Pass "<<i<<":";
+        for (int i = 0; i < 10; i++) cout << d[i] << " ";
         cout<<endl;
     } 
 } 
 
-
-
 int main(){
-	int a[10] = {12,25,30,44,2,0,4,7,55,25};
-	cout << "Input Array:";
-	for(int i = 0; i < 10; i++) cout << a[i] << " ";
-	cout << "\n\n";
-	
-	insertionSort(a,10);
-	
-	cout << "\nSorted Array:";
-	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
+    int a[10] = {12,25,30,44,2,0,4,7,55,25};
+    cout << "Input Array:";
+    for(int i = 0; i < 10; i++) cout << a[i] << " ";
+    cout << "\n\n";
+
+    insertionSort(a,10);
+
+    cout << "\nSorted Array:";
+    for(int i = 0; i < 10; i++) cout << a[i] << " ";
 }
